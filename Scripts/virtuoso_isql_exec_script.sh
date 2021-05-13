b8c0e41bb7a354eccaa4613186459dbdd6c3b1d1
161905 <cristobal.carmona.14@sansano.usm.cl>
2021-05-13 18:49:52 -0400

    Se modificaron los scripts para que ahora lea a partir de un archivo. Además se ingreso data para que entrege respuestas (esto es fuera del repositorio, en virtuoso) , y se guarda ahora en un archivo toda las respuestas de una determinada consulta en una carpeta particular.

--- Scripts/virtuoso_isql_exec_script.sh
+++ Scripts/virtuoso_isql_exec_script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 system_password="161905";
 current_path="/home/c161905/Memoria/memoria_utfsm_sparql"
 path_virtuoso_isql=$VIRTUOSO7_isql;
@@ -9,7 +8,33 @@ isql_host="1111";
 isql_username="dba";
 isql_password="dba";
 
-str_sparql='select distinct ?Concept where {[] a ?Concept} LIMIT 100'
+str_sparql_file=$(<sparql_files/$1)
+
+
+
+if [[ ! -f "sparql_files/$1" ]] ; then
+    echo 'File is not there, aborting.'
+    exit
+fi
+
+mkdir -p outputs
+cd outputs
+
+
+
+#str_sparql='select distinct ?Concept where {[] a ?Concept} LIMIT 100'
+str_sparql=$str_sparql_file
+
+
+file_from_arg=$1
+filename="${file_from_arg%.*}"
+mkdir -p outputs_$filename
+cp "$current_path/Scripts/sparql_files/$1" outputs_$filename
+
+sparql_execution="
+SPARQL $str_sparql;
+"
+echo "$sparql_execution"
 
 sparql_translate="
 SET BLOBS ON;
@@ -29,15 +54,17 @@ SET BLOBS ON;
 profile('SPARQL $str_sparql');
 "
 
-mkdir -p outputs
+
 
 echo "Executando isql";
-echo -e $system_password | sudo -S $path_virtuoso_isql $isql_host $isql_username $isql_password VERBOSE=OFF BANNER=OFF exec="$sparql_translate" > outputs/sparql_translate_file
-echo -e $system_password | sudo -S $path_virtuoso_isql $isql_host $isql_username $isql_password VERBOSE=OFF BANNER=OFF exec="$explain_normal" > outputs/explain_normal_file
-echo -e $system_password | sudo -S $path_virtuoso_isql $isql_host $isql_username $isql_password VERBOSE=OFF BANNER=OFF exec="$profile_normal" > outputs/profile_normal_file
+echo -e $system_password | sudo -S $path_virtuoso_isql $isql_host $isql_username $isql_password exec="$sparql_execution"
+echo -e $system_password | sudo -S $path_virtuoso_isql $isql_host $isql_username $isql_password exec="$sparql_execution" > outputs_$filename/sparql_execution_file_$filename
+echo -e $system_password | sudo -S $path_virtuoso_isql $isql_host $isql_username $isql_password VERBOSE=OFF BANNER=OFF exec="$sparql_translate" > outputs_$filename/sparql_translate_file_$filename
+echo -e $system_password | sudo -S $path_virtuoso_isql $isql_host $isql_username $isql_password VERBOSE=OFF BANNER=OFF exec="$explain_normal" > outputs_$filename/explain_normal_file_$filename
+echo -e $system_password | sudo -S $path_virtuoso_isql $isql_host $isql_username $isql_password VERBOSE=OFF BANNER=OFF exec="$profile_normal" > outputs_$filename/profile_normal_file_$filename
 
 
-sparql_translate_file=$(cat outputs/sparql_translate_file)
+sparql_translate_file=$(cat outputs_$filename/sparql_translate_file_$filename)
 
 sparql_explain="
 SET EXPLAIN ON;
@@ -51,7 +78,7 @@ $sparql_translate_file;
 SET PROFILE OFF;
 "
 
-echo -e $system_password | sudo -S $path_virtuoso_isql $isql_host $isql_username $isql_password VERBOSE=OFF BANNER=OFF exec="$sparql_explain" > outputs/sparql_explain_file
-echo -e $system_password | sudo -S $path_virtuoso_isql $isql_host $isql_username $isql_password VERBOSE=OFF BANNER=OFF exec="$sparql_profile" > outputs/sparql_profile_file
+echo -e $system_password | sudo -S $path_virtuoso_isql $isql_host $isql_username $isql_password VERBOSE=OFF BANNER=OFF exec="$sparql_explain" > outputs_$filename/sparql_explain_file_$filename
+echo -e $system_password | sudo -S $path_virtuoso_isql $isql_host $isql_username $isql_password VERBOSE=OFF BANNER=OFF exec="$sparql_profile" > outputs_$filename/sparql_profile_file_$filename
 
 
