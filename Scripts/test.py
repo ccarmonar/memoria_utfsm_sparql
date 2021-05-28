ff7a8b51f4db9e99b44a86b808b6ca2359d37845
161905 <cristobal.carmona.14@sansano.usm.cl>
2021-05-28 17:51:56 -0400

    Actualizacion script de extraccion de caracteristicas: IdentifyOperatorType sirve para identificar que tipo de operador es. CleanOperators es para limpiar el diccionario de profile_text vacios

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,5 +1,6 @@
 import re, time
-z =37
+#z es el indice de el archivo de la lista n que se quiere abrir
+z = 37
 n = [
 	'ex003',#0
 	'ex006',#1
@@ -43,9 +44,8 @@ n = [
 	
 	]
 
-
+#PATHS
 output_path = "outputs/outputs_" + n[z]
-#print(output_path)	
 
 path_sparql_file = output_path + "/" + n[z] + ".rq"
 
@@ -59,29 +59,13 @@ path_explain_order_loop = output_path + "/explain_order_loop_file_" + n[z]
 
 path_translate_sparql = output_path + "/sparql_translate_file_" + n[z]
 
-#print(path_sparql_file)
-
-#print(path_profile_sparql)
-#print(path_profile_explain_bajo_sparql)
-#print(path_profile_loop_sparql)
-#print(path_profile_order_loop_sparql)
-
-#print(path_explain_sparql)
-#print(path_explain_order_loop)
-
-#print(path_translate_sparql)
-#print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+#FILES
 sparql_file = open(path_sparql_file, 'r').read()
 profile_sparql = open(path_profile_sparql, 'r').read()
 profile_explain_bajo_sparql = open(path_profile_explain_bajo_sparql, 'r').read()
 translate_sparql = open(path_translate_sparql,'r').read()
-print(sparql_file)
-print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
-print(profile_sparql)
-print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
-print(profile_explain_bajo_sparql)
-print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
 
+#Funcion que sirve para entrar dentro de los {} según nivel en un string. Esta enfocado a JSON pero sirve para al comienzo.
 def ParseNestedBracket(string, level):
     """
     Return string contained in nested {}, indexing i = level
@@ -118,29 +102,18 @@ def GroupOperators(profile_sparql):
 		else:
 			operators[OP]['profile_text']=operators[OP]['profile_text']+'\n'+x
 		
-		#print(x)
-		#print(OP)
 	return operators
-	
-operators = GroupOperators(profile_sparql)
 
-#print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
-#for k,v in operator.items():
-#	print(k)
-#	print(v)
-print("||||||||+++++|||||||")
-print(operators["OP0"])
-print(operators["OP2"])
-print(operators["OP1"])
-print("||||||||+++++|||||||")
-print("||||||||+++++|||||||")
-print(operators["OP0"]['profile_text'])
-print(operators["OP2"]['profile_text'])
-print(operators["OP1"]['profile_text'])
-print("||||||||+++++|||||||")
+#Funcion que hace una limpieza y saca los operadores que tengan texto vacio
+def CleanOperators(operators):
+	remove_list = []
+	for k in operators:
+		if operators[k]['profile_text'] == '':
+			remove_list.append(k)
+	for k in remove_list:
+		operators.pop(k)
+	return operators
 
-def IdentifyOperatorType(operator):
-	return 0
 
 #Esta función obtiene el time, fanout, input rows y estimacion de cardinalidad (si lo dispone Virtuoso, si compete y/o simplemente de existir) de un operador
 #Crea un nuevo diccionario con estos valores
@@ -162,13 +135,11 @@ def GetOperatorExecutionFeatures(operator):
 			for nch in range(0,len(short_l_filt)):
 				if short_l_filt[nch] == 'Cardinality' and nch == 0 and short_l_filt[nch+1] == "estimate:":
 					return_dicto['cardinality_estimate'] = short_l_filt[nch+2]
-				if short_l_filt[nch] == 'Fanout' and nch == 3:
+				if short_l_filt[nch] == 'Fanout:' and nch == 3:
 					return_dicto['cardinality_fanout'] = short_l_filt[nch+1]
 	return return_dicto	
 
-print(GetOperatorExecutionFeatures(operators["OP2"]))
-print("||||||||+++++|||||||")
-print(GetOperatorExecutionFeatures(operators["OP0"]))
+
 #Obtiene un diccionario con los PREFIX de la consulta Sparql
 def GetPrefixes(sparql_file):
 	dicto = {}
@@ -183,19 +154,26 @@ def GetPrefixes(sparql_file):
 					dicto[ls[1][:-1]] = ls[2]
 	return dicto
 
-#Obtiene las traducciones de SPARQL->SQL de Virtuoso para IRI/URIs, Literales y  Variables
-def GetTranslates(sparql_file, translate_sparql):
-	lines_split_translate_text = translate_sparql.split("\n")
-	lines_split_sparql_file_text = sparql_file.split("\n")
-	
-	for lt in lines_split_translate_text:
-		print(lt)
-	
-	
-	return 0
-	
-#print(GetPrefixes(sparql_file))
-#print(GetTranslates(sparql_file,translate_sparql))
+#Función que identifica si el operador es SCAN, SUBQUERY, u otro.
+def IdentifyOperatorType(operator):
+	if "P =  " and "Key RDF_QUAD" and "from DB.DBA.RDF_QUAD by RDF_QUAD" in operator['profile_text']:
+		operator['operator_type'] = 'scan'
+	else:
+		operator['operator_type'] = 'no_scan'
+	return operator
 
-#print(GetTranslates(sparql_file,translate_sparql))
+def execute(profile_sparql):
+	operators = GroupOperators(profile_sparql)
+	operators = CleanOperators(operators)
+	for i in operators.keys():
+		operators[i] = GetOperatorExecutionFeatures(operators[i])
+		operators[i] = IdentifyOperatorType(operators[i])
+	#print(operators["OP5"])	
+	return operators
 
+operators = execute(profile_sparql)
+
+#print(operators)
+for k,v in operators.items():
+	print("   ")	
+	print(k,v)
