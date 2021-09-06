1eedf1e187c35c1e8509d935087f5f3e5dfdffaf
161905 <cristobal.carmona.14@sansano.usm.cl>
2021-09-06 16:55:39 -0300

    Actualización extracción de características: - Rework de TreeFormat, Parte 8: Se desarrollo la uníon entre BGPs. También ahora se discretiza en caso de que solo eixsta un bgp para que no este dentro de una lista de listas. Para esto fue necesario desarrollar y modificar varias funciones auxiliares como IterateBuildTree, InnerJoinsIntraBGPS, IterateBuildTreeBetweenBGPS y obviamente TreeFormat

--- scripts/feature_extraction_script/functions/tree_format.py
+++ scripts/feature_extraction_script/functions/tree_format.py
@@ -3,43 +3,78 @@ from anytree.exporter import DotExporter
 from functions.aux import OnlyScans
 
 
-def IterateBuildTree(prearmed, binary_tree_format):
+def IterateBuildTree(tree_format, prearmed):
     if len(prearmed) == 0:
-        return binary_tree_format,prearmed
+        return tree_format,prearmed
     if len(prearmed) == 1:
-        binary_tree_format = prearmed
+        tree_format = prearmed
         prearmed = []
-        return binary_tree_format,prearmed
+        return tree_format,prearmed
     else:
-        if len(binary_tree_format) == 0:
+        if len(tree_format) == 0:
             aux = [prearmed[1],[prearmed[0], prearmed[2]]]
             prearmed = prearmed[3::]
         else:
-            aux = [prearmed[0],binary_tree_format,prearmed[1]]
+            aux = [prearmed[0],tree_format,prearmed[1]]
             prearmed = prearmed[2::]
-    return IterateBuildTree(prearmed, aux)
-
+    return IterateBuildTree(aux, prearmed)
 
 
 def InnerJoinsIntraBGPS(bgp):
     prearmed = []
-    binary_tree_format = []
-
+    tree_format = []
     for k in range(len(bgp)):
         if k == 0:
-            prearmed.append(bgp[k]['OP']+"|"+bgp[k]['P'])
+            prearmed.append(bgp[k]['triple_type']+"|"+bgp[k]['P'])
         else:
             prearmed.append('JOIN')
-            prearmed.append(bgp[k]['OP'] + "|" + bgp[k]['P'])
-    binary_tree_format, prearmed = IterateBuildTree(prearmed, binary_tree_format)
-    return binary_tree_format
+            prearmed.append(bgp[k]['triple_type'] + "|" + bgp[k]['P'])
+    tree_format, prearmed = IterateBuildTree(tree_format, prearmed)
+    return tree_format
+
+
+def IterateBuildTreeBetweenBGPS(tree_format, prearmed):
+    if len(prearmed) == 0:
+        return tree_format,prearmed
+    if len(prearmed) == 1:
+        tree_format = prearmed
+        prearmed = []
+        return tree_format,prearmed
+    else:
+        if len(tree_format) == 0:
+            aux = [prearmed[1],prearmed[0], prearmed[2]]
+            prearmed = prearmed[3::]
+        else:
+            aux = [prearmed[0],tree_format,prearmed[1]]
+            prearmed = prearmed[2::]
+    return IterateBuildTreeBetweenBGPS(aux, prearmed)
 
 
 def TreeFormat(operators, sparql_file):
     bgp_joins = []
+    bgp_type = []
     tree_format = []
-    for k in operators['GF_FROM_OP']['bgps_ops']:
-        x = InnerJoinsIntraBGPS(operators['GF_FROM_OP']['bgps_ops'][k])
-        bgp_joins.append(x)
+    prearmed = []
+    for k,v in operators['GF_FROM_OP']['bgps_ops'].items():
+        bgp = InnerJoinsIntraBGPS(v['bgp_list'])
+        type = v['opt']
+        bgp_joins.append(bgp)
+        bgp_type.append(type)
 
-    return 0
+    for k in range(len(bgp_joins)):
+        if k == 0:
+            prearmed.append(bgp_joins[k])
+        else:
+            if bgp_type[k] == 0:
+                prearmed.append('JOIN')
+                prearmed.append(bgp_joins[k])
+            if bgp_type[k] == 1:
+                prearmed.append('LEFT JOIN')
+                prearmed.append(bgp_joins[k])
+    if operators['GF_FROM_OP']['total_bgps'] == 1:
+        tree_format = prearmed[0]
+    else:
+        tree_format, prearmed = IterateBuildTreeBetweenBGPS(tree_format, prearmed)
+    print(tree_format)
+
+    return tree_format
