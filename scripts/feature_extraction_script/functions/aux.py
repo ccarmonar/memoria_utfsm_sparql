11f9393db6d4e218046f9cdce1a5255d9ff48527
161905 <cristobal.carmona.14@sansano.usm.cl>
2021-11-12 17:48:49 -0300

    Actualización extracción de características: - Se corrigio un error en GetJsonPredicatesFeatures ya que no consideraba si un predicado podia aparecer dos veces. Para eso ahora el GetJsonPredicatesFeatures genera varios jsons considerando distintas maneras de tratar el problema (sobrescribir, promedio, suma, o una label para diferenciar uno con el otro)

--- scripts/feature_extraction_script/functions/aux.py
+++ scripts/feature_extraction_script/functions/aux.py
@@ -141,4 +141,18 @@ def OnlyScans(operators, set_nones_predicates):
 
 
 def Flatten(t):
-    return [item for sublist in t for item in sublist]
+	return [item for sublist in t for item in sublist]
+
+
+def ListDuplicatesOf(seq,item):
+	start_at = -1
+	locs = []
+	while True:
+		try:
+			loc = seq.index(item,start_at+1)
+		except ValueError:
+			break
+		else:
+			locs.append(loc)
+			start_at = loc
+	return locs
